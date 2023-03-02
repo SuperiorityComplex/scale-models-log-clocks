affa40d1b9448d51422751aa15f9a866e038a578
Ivan Zhang <ivanzhangofficial@gmail.com>
2023-03-01 21:26:09 -0500

    add graceful shutdown and duration

--- main.py
+++ main.py
@@ -10,6 +10,9 @@ running_threads = []
 # Stores all the sockets that are running. (Used for graceful shutdown)
 running_sockets = []
 
+# Event that is set when threads are running and cleared when you want threads to stop
+run_event = threading.Event()
+
 def get_sys_args():
     """
     Gets the arguments from the flags
@@ -21,11 +24,13 @@ def get_sys_args():
     p = optparse.OptionParser()
     p.add_option('--log_clock', '-l', default="6")
     p.add_option('--act_range', '-a', default="6")
+    p.add_option('--duration', '-d', default="5")
     options, _ = p.parse_args()
     log_clock_val = int(options.log_clock)
     act_value = int(options.act_range)
+    duration = int(options.duration)
 
-    return log_clock_val, act_value
+    return log_clock_val, act_value, duration
 
 # def write_message_to_socket(message, source_thread, dest_thread):
 #     """
@@ -38,14 +43,16 @@ def get_sys_args():
 #     """
 #     return
 
-# def init_log_files(base_file_name="log"):
-#     """
-#     Initializes the log files for each thread.
-#     @Parameter: 
-#     - base_file_name: The base file name for the log files.
-#     @Returns: None.
-#     """
-#     return
+def init_log_file(thread_id, base_file_name="thread"):
+    """
+    Initializes the log files for each thread.
+    @Parameter: 
+    - thread_id: The id of the thread (0, 1, 2).
+    - base_file_name: The base file name for the log files.
+    @Returns: None.
+    """
+    with open("logs/{}".format("{}_{}".format(base_file_name, str(thread_id))), "a+"):
+        return
 
 # def write_log_to_file():
 #     """
@@ -63,7 +70,8 @@ def get_sys_args():
 #     """
 #     return
 
-def read_message_from_socket(socket, network_queue):
+
+def read_message_from_socket(sock, network_queue):
     """
     Reads a message from a socket and save it to the network queue.
     @Parameter:
@@ -71,9 +79,10 @@ def read_message_from_socket(socket, network_queue):
     - network_queue: The network queue to save the message to.
     @Returns: None.
     """
-    while True: 
-         message = socket.recv(1024).decode('utf-8')
-
+    while run_event.is_set(): 
+        # Message is just the local logical clock time
+        message = sock.recv(1024).decode('utf-8')
+        network_queue.append(message)
     return
     
 
@@ -92,16 +101,33 @@ def listen_for_connections(sock: socket.socket):
         running_sockets.append(client_socket)
         num_connected += 1
 
-def start_thread_process(thread_id, clock_val):
+
+def do_thread_actions():
+    """
+    Performs the actions of the thread.
+    @Parameter: None.
+    @Returns: None.
+    """
+        # Get the arguments from the flags
+#     log_clock_val, act_value, _ = get_sys_args()
+# clock_val = randrange(log_clock_val)
+    while run_event.is_set():
+        print( "HI")
+    return
+
+def thread_process(thread_id):
     """
     Defines the actions of the thread process.
     @Parameter:
     - thread_id: The id of the thread (0, 1, 2).
-    - clock_val: The number of clock ticks per (real world) second.
     @Returns: None.
     """
+
     # Initialize the network queue for the process
     network_queue = []
+
+    # Initialize the log file for the process
+    init_log_file(thread_id)
     
     # Create socket to listen for messages for this process
     sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
@@ -113,7 +139,7 @@ def start_thread_process(thread_id, clock_val):
     accept_thread = threading.Thread(
                 target=listen_for_connections, args=(sock,))
     accept_thread.start()
-    running_threads.insert(0, accept_thread)
+    running_threads.append(accept_thread)
 
     # Ensure that all sockets are connected for writing
     connected_sockets = []
@@ -125,16 +151,12 @@ def start_thread_process(thread_id, clock_val):
         connected_sockets.append(c_socket)
         running_sockets.append(sock)
 
-    print(connected_sockets)
-    
     # Create constantly running thread to read message from socket into network_queue
-    # read_msg_thread = threading.Thread(
-    #             target=read_message_from_socket, args=(sock, network_queue,))
-    # read_msg_thread.start()
-    # running_threads.insert(0, read_msg_thread)
-
-
-    return
+    read_msg_thread = threading.Thread(
+                target=read_message_from_socket, args=(sock, network_queue,))
+    read_msg_thread.start()
+    running_threads.append(read_msg_thread)
+    do_thread_actions()
 
 def gracefully_shutdown():
     """
@@ -142,33 +164,35 @@ def gracefully_shutdown():
     @Parameter: None.
     @Returns: None.
     """
+    run_event.clear()
+    print("SHITDOWN")
     try:
+        for sock in running_sockets:
+            sock.shutdown(socket.SHUT_RDWR)
         for thread in running_threads:
             thread.join()
-        for socket in running_sockets:
-            socket.shutdown(socket.SHUT_RDWR)
     except (OSError):
         # This occurs when the socket is already closed.
         pass
     print("threads and sockets successfully closed.")
     sys.exit(0)
 
-def start_threads(log_clock_val):
+def start_threads():
     """
     Starts the threads.
-    @Parameter:
-    - log_clock_val: The number of clock ticks per (real world) second.
+    @Parameter: None.
     @Returns: None.
     """
     try:
+        run_event.set()
         thread0 = threading.Thread(
-            target=start_thread_process, args=(0, randrange(log_clock_val)))
+            target=thread_process, args=(0,))
         thread0.start()
         thread1 = threading.Thread(
-            target=start_thread_process, args=(1, randrange(log_clock_val)))
+            target=thread_process, args=(1,))
         thread1.start()
         thread2 = threading.Thread(
-            target=start_thread_process, args=(2, randrange(log_clock_val)))
+            target=thread_process, args=(2,))
         thread2.start()
         running_threads.append(thread0)
         running_threads.append(thread1)
@@ -178,8 +202,10 @@ def start_threads(log_clock_val):
         gracefully_shutdown()
 
 def main():
-    log_clock_val, act_value = get_sys_args()
-    start_threads(log_clock_val)
+    _, _, duration = get_sys_args()
+    shutdown_thread = threading.Timer(duration, gracefully_shutdown)
+    shutdown_thread.start()
+    start_threads()
 
 
 main()
